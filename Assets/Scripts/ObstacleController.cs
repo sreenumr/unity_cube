9b564cd9690f8cfcad82f980b71ea062dd8e9e30
sreenumr <sreenu959@gmail.com>
2020-11-01 11:06:13 +0530

    Added up/down movement for obstacles

--- Assets/Scripts/ObstacleController.cs
+++ Assets/Scripts/ObstacleController.cs
@@ -6,18 +6,33 @@ public class ObstacleController : MonoBehaviour
 {
     // Start is called before the first frame update
     
-    [SerializeField]public float speed = 40f;
-    [SerializeField]public float upSpeed = 40f;
+    [SerializeField]public float speed = 25f;
+    [SerializeField]public float ySpeed = 2f;
+    public float maxHeight;
+    public float minHeight;
+    private Rigidbody2D rigidbody2d;
+
 
     void Start()
     {
-        
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         transform.position += Vector3.left * speed * Time.deltaTime;
-        // transform.position += Vector3.up * upSpeed * Time.deltaTime;
+        Debug.Log(transform.position.y);
+
+        transform.position -= Vector3.up * ySpeed * Time.deltaTime;
+
+        
+        if(transform.position.y > maxHeight || transform.position.y < minHeight){
+            ySpeed *= -1;
+        }
+
+    }
+
+    void Update(){
+
     }
 }
