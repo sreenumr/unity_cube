af84ef061b750d47b0ba933ebd369e9d694513d8
sreenumr <sreenu959@gmail.com>
2020-12-30 18:42:14 +0530

    Played sound on score

--- Assets/Scripts/AddScore.cs
+++ Assets/Scripts/AddScore.cs
@@ -5,10 +5,20 @@ using UnityEngine;
 public class AddScore : MonoBehaviour
 {
 
+    // public AudioClip scoredAudio;
+    private AudioSource audioSource;
+
     private bool isHighScore = false;
+
+    void Start(){
+        audioSource = GetComponent<AudioSource> ();
+    }
+
     private void OnTriggerEnter2D(Collider2D collision){
         if(collision.gameObject.layer == 9){
             Score.score++;
+                    
+            audioSource.Play();
 
             if(Score.score > HighScore.highScore){
                 isHighScore = true;
